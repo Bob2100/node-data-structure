1e7a113ce2ebd890952dc5267f91925781aace26
wenbo.wu <bob_w@qq.com>
2022-08-11 09:50:17 +0800

    Create StackList.js

---
+++ StackList.js
@@ -0,0 +1,45 @@
+const VALUE = Symbol('value')
+const HANDLERS = Symbol('handlers')
+
+export default class StackList {
+  constructor(...args) {
+    this[VALUE] = args || []
+    this[HANDLERS] = {}
+  }
+  get value() {
+    return [...this[VALUE]]
+  }
+  addEventListener(event, handler) {
+    let handlers = this[HANDLERS][event]
+    if (!handlers) {
+      handlers = this[HANDLERS][event] = []
+    }
+    handlers.push(handler)
+  }
+  removeEventListener(event, handler) {
+    let handlers = this[HANDLERS][event]
+    if (!handlers) {
+      return
+    }
+    const index = handlers.findIndex((item) => item === handler)
+    if (index >= 0) {
+      handlers.splice(index, 1)
+    }
+  }
+  push(...args) {
+    const handlers = this[HANDLERS]['push']
+    handlers.forEach((item) => {
+      item.apply(this, args)
+    })
+    return this[VALUE].push(...args)
+  }
+  pop() {
+    return this[VALUE].pop()
+  }
+  clear() {
+    this[VALUE].length = 0
+  }
+  size() {
+    return this[VALUE].length
+  }
+}
